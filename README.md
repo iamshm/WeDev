cdca9ca82b7bf4c2f5c3ad21a30bc6d775750a98
Suraj Maharana <43462941+iamshm@users.noreply.github.com>
2020-09-26 02:22:32 +0530

    Login added

--- README.md
+++ README.md
@@ -8,12 +8,20 @@
 6. Use the User model in user route and register new User
 7. For registerising User we have to check if user already exists
 8. if new user we will encrypt password using bcrypt and then save it to mongoDB
-9. Then provide a token to the user(It can be a new user or existing user)
+9. Then provide a token to the user(It can be a new user or existing user) \
    a. We will then use user-id to provide token \
    b. To create a token which can be then send to a authorized route, we have to create it usinq `jwt.sign()` we will send
    `user.id` as payload \
    c. Then we use that token for our middleware(in middleware we are just decrypting the token and assigning `req.user` to \
-    the user in database) which will provide access to authorized routes \
+    the user in database) which will provide access to authorized routes
 10. After using that token for our middleware we can use the middleware as a 2nd Parameter to our auth-route, \
     which will make the auth route a procted/authorized route.
 11. Then that route can display all user data.
+
+```
+Login/Register Summary
+if its a new user, first it is saved to database and then it is given a token which is then decoded ny middleware and
+then authorized by auth route for access its details.
+if its an existing user, it's credential are validated and then a token is provided to him/her and then decoded then
+authorized by auth route to access its details
+```
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,9 +2,13 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/auth");
 const User = require("../../models/User");
+const { check, validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const bycrpt = require("bcryptjs");
 
 // @route  GET api/auth
-// @desc   Test route
+// @desc   Auth route (Resgistration middleware)
 // @access Protected
 router.get("/", auth, async (req, res) => {
   try {
@@ -15,5 +19,62 @@ router.get("/", auth, async (req, res) => {
     res.status(500);
   }
 });
+
+// @route  POST api/auth
+// @desc   Authenticate User and get token(Login middleaware)
+// @access Public
+router.post(
+  "/",
+  [
+    check("email", "Not a valid Email").isEmail(),
+    check("password", "Password is Required").exists(),
+  ],
+  async (req, res) => {
+    //checking errors in body
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      //Check if user exists
+      let user = await User.findOne({ email: email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid Credentials" }] });
+      }
+      //checking if password is correct
+      const isMatch = await bycrpt.compare(password, user.password);
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid Credentials" }] });
+      }
+      //Return JWT token
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+
+      //res.send(`User Registered`);
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
 
 module.exports = router;
