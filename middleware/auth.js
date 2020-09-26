8be7e87e3d82a0d0e63fe2fd786fd2df90acfc6e
Suraj Maharana <43462941+iamshm@users.noreply.github.com>
2020-09-26 23:01:49 +0530

    Creating Profile API

--- middleware/auth.js
+++ middleware/auth.js
@@ -10,6 +10,7 @@ module.exports = function (req, res, next) {
   try {
     const decoded = jwt.verify(token, config.get("jwtSecret"));
     req.user = decoded.user;
+    console.log(req.user.id);
     next();
   } catch (err) {
     res.status(401).json({ msg: "Token Invalid" });
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,69 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+const { check, validationResult } = require("express-validator");
+// @route  GET api/profile/me
+// @desc   Get current users profile
+// @access Private
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id,
+    }).populate("user", ["name", "avatar"]);
+    if (!profile) {
+      res.status(400).send({ msg: "User has No Profile" });
+      res.json(profile);
+    }
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
-// @route  GET api/profile
-// @desc   Test route
-// @access Public
-router.get("/", (req, res) => res.send("Profile route"));
-
+// @route  POST api/profile
+// @desc   Create or Update User Profile
+// @access Private
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("status", "Status is Required").not().isEmpty(),
+      check("skills", "Skills is Required").not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errorsFromValidator = validationResult(req);
+    if (!errorsFromValidator.isEmpty()) {
+      res.status(400).json({
+        errors: errorsFromValidator.array(),
+      });
+    }
+    const {
+      company,
+      website,
+      location,
+      status,
+      skills,
+      bio,
+      githubUserName,
+      experience,
+      youtube,
+      facebook,
+      linkedin,
+      twitter,
+      instagram,
+    } = req.body;
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (company) profileFields.company = company;
+    if (website) profileFields.website = website;
+    if (bio) profileFields.bio = bio;
+    if (location) profileFields.location = location;
+    if (status) profileFields.status = status;
+    if (githubUserName) profileField.githubUserName = githubUserName;
+  }
+);
 module.exports = router;
