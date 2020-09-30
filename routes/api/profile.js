20157e572bb71a504052823595637e8e41d28d56
Suraj Maharana <43462941+iamshm@users.noreply.github.com>
2020-09-30 22:24:11 +0530

    Github functionality added not working yrt :(

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const auth = require('../../middleware/auth');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
-const { check, validationResult } = require('express-validator');
+const { check, validationResult, body } = require('express-validator');
+const request = require('request');
+const config = require('config');
 
 // @route  GET api/profile/me
 // @desc   Get current users profile
@@ -286,6 +288,36 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
 		profile.experience.splice(removeIndex, 1);
 		await profile.save();
 		res.status(200).json(profile);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
+});
+
+// @route  GET api/profile//github/:username
+// @desc   Get reposiritories from Github
+// @access Public
+router.get('/github/:username', async (req, res) => {
+	try {
+		const options = {
+			uri: `https://api.github.com/users/${req.params.username}/
+            repos?per_page=5&sort=created:asc&
+            client_id=${config.get(
+				'githubClientId'
+			)}&client_secret=${config.get('githubClientSecret')}`,
+			method: 'GET',
+			headers: { 'user-agent': 'node.js' },
+		};
+		request(options, (error, response, body) => {
+			if (error) {
+				return console.error(error.message);
+			}
+			if (response.statusCode !== 200)
+				return res.status(404).json({
+					msg: 'No Github Profile Found with this username',
+				});
+			res.json(JSON.parse(body));
+		});
 	} catch (err) {
 		console.error(err.message);
 		res.status(500).send('Server Error');
