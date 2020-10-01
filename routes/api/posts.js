c198e6092631b2acfc809e2f04bdb35c8c7adae1
Suraj Maharana <43462941+iamshm@users.noreply.github.com>
2020-10-01 22:39:45 +0530

    Post Model created

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,22 @@
-const express = require("express");
+const express = require('express');
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
 const router = express.Router();
 
-// @route  GET api/posts
+// @route  POST api/posts
 // @desc   Test route
 // @access Public
-router.get("/", (req, res) => res.send("Post route"));
+router.post(
+	'/',
+	[auth, [check('text', 'Text is Required').not().isEmpty()]],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({
+				errors: errors.array(),
+			});
+		}
+	}
+);
 
 module.exports = router;
